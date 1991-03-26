273082d0f3568bc58675dc20522e0266c4e81ec3
Christos Zoulas <christos@zoulas.com>
1991-03-26 03:22:50 +0000

    AT&T clean version

---
+++ patchlevel.h
@@ -0,0 +1,15 @@
+/* $Header$ */
+/*
+ * patchlevel.h: Our life story.
+ */
+#ifndef _h_patchlevel
+#define _h_patchlevel
+
+#define ORIGIN "Cornell"
+#define REV 5
+#define VERS 99
+#define PATCHLEVEL 0
+#define DATE "02/22/91"
+#define LOCALSTR "(beta)"
+
+#endif /* _h_patchlevel */
