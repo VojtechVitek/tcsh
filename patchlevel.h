1b5468e56b5af6a924dd2c99592ce68966856d3f
Christos Zoulas <christos@zoulas.com>
1995-05-13 20:23:31 +0000

    *** empty log message ***

--- patchlevel.h
+++ patchlevel.h
@@ -7,8 +7,8 @@
 
 #define ORIGIN "Cornell"
 #define REV 6
-#define VERS 5
-#define PATCHLEVEL 9
-#define DATE "1995-05-06"
+#define VERS 6
+#define PATCHLEVEL 0
+#define DATE "1995-05-13"
 
 #endif /* _h_patchlevel */
