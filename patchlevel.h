5eed78bc9d69767328e937044f471161ad435576
Christos Zoulas <christos@zoulas.com>
1996-09-25 01:58:17 +0000

    Fixed patchlevel

--- patchlevel.h
+++ patchlevel.h
@@ -5,9 +5,9 @@
 #ifndef _h_patchlevel
 #define _h_patchlevel
 
-#define ORIGIN "D.E.Shaw"
+#define ORIGIN "Astron"
 #define REV 6
-#define VERS 0
+#define VERS 6
 #define PATCHLEVEL 3
 #define DATE "1996-09-24"
 
