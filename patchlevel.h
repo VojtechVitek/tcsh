b852f1a9425d4e3b67eba28d0931cca43ce10f4a
Christos Zoulas <christos@zoulas.com>
1999-05-11 13:22:46 +0000

    more commits.

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 8
-#define PATCHLEVEL 4
-#define DATE "1999-04-19"
+#define PATCHLEVEL 5
+#define DATE "1999-05-11"
 
 #endif /* _h_patchlevel */
