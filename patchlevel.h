427ab0d8f69fc5926805f6506e28ab7e5d1fc7fc
Christos Zoulas <christos@zoulas.com>
1999-08-13 16:40:05 +0000

    get ready for 6.08.07

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 8
-#define PATCHLEVEL 6
-#define DATE "1999-07-01"
+#define PATCHLEVEL 7
+#define DATE "1999-08-13"
 
 #endif /* _h_patchlevel */
