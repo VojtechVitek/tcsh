7cfc9c25e0010e90ecb73bdfca1172d5fcfb78a4
Christos Zoulas <christos@zoulas.com>
2001-07-25 15:48:04 +0000

    open heredoc temp file 600

--- sh.dol.c
+++ sh.dol.c
@@ -1043,7 +1043,7 @@ again:
 #ifndef O_EXCL
 # define O_EXCL 0
 #endif
-    if (open(tmp, O_RDWR|O_CREAT|O_EXCL|O_TEMPORARY) == -1) {
+    if (open(tmp, O_RDWR|O_CREAT|O_EXCL|O_TEMPORARY, 0600) == -1) {
 	int oerrno = errno;
 #ifndef WINNT_NATIVE
 	if (errno == EEXIST) {
