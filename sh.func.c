b667f8f2254563640cd8be03eb5236d2e189d45d
Christos Zoulas <christos@zoulas.com>
2002-03-08 17:36:45 +0000

    remove clause 3 of the copyright.

--- sh.func.c
+++ sh.func.c
@@ -14,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -1382,6 +1378,11 @@ dosetenv(v, c)
 	return;
     }
 
+    if (eq(vp, STRNLSPATH)) {
+	(void) catclose(catd);
+	nlsinit();
+    }
+
     if (eq(vp, STRNOREBIND)) {
 	NoNLSRebind = 1;
 	MapsAreInited = 0;
@@ -1576,6 +1577,10 @@ dounsetenv(v, c)
 		else if (eq(name, STRLS_COLORS))
 		    parseLS_COLORS(n);
 #endif /* COLOR_LS_F */
+		else if (eq(name, STRNLSPATH)) {
+		    (void) catclose(catd);
+		    nlsinit();
+		}
 		/*
 		 * start again cause the environment changes
 		 */
--- tc.func.c
+++ tc.func.c
@@ -14,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -1070,8 +1066,11 @@ job_cmd(args)
 	goto leave;
     }
     jobcmd_active = 1;
-    if (!whyles && adrof1(STRjobcmd, &aliases))
+    if (!whyles && adrof1(STRjobcmd, &aliases)) {
+	struct process *pp = pcurrjob; /* put things back after the hook */
 	aliasrun(2, STRjobcmd, args);
+	pcurrjob = pp;
+    }
 leave:
     jobcmd_active = 0;
 #ifdef BSDSIGS
