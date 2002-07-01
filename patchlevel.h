0a52811079f4eeb2bff30660fead476852be70ba
Christos Zoulas <christos@zoulas.com>
2002-07-01 21:07:44 +0000

    goto 11.03.

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 11
-#define PATCHLEVEL 02
-#define DATE "2002-05-16"
+#define PATCHLEVEL 03
+#define DATE "2002-07-01"
 
 #endif /* _h_patchlevel */
