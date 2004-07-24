919132adc606238d49bbddffd03d77b597bccd49
Christos Zoulas <christos@zoulas.com>
2004-07-24 22:10:34 +0000

    welcome to 6.13.01

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 13
-#define PATCHLEVEL 00
-#define DATE "2004-05-19"
+#define PATCHLEVEL 01
+#define DATE "2004-07-24"
 
 #endif /* _h_patchlevel */
