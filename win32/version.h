2945a4374000ec1d253029dd70c18a21f3afec1f
Amol Deshpande <amold@microsoft.com>
2004-10-18 02:32:17 +0000

    Fix hangs because of lost SIGCHLD.

--- win32/version.h
+++ win32/version.h
@@ -5,9 +5,9 @@
 /* remember to change both instance of the version -amol */
 
 #ifdef NTDBG
-#define LOCALSTR ",nt-rev-7.10-debug"
+#define LOCALSTR ",nt-rev-7.11-debug"
 #else
-#define LOCALSTR ",nt-rev-7.10" 
+#define LOCALSTR ",nt-rev-7.11" 
 								//patches
 #endif NTDBG
 
