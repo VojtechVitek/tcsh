08e5890b9ade1e3e9de77a8d03298c28468c02b2
Kimmo Suominen <kim@tcsh.org>
2005-03-03 00:43:58 +0000

    Prepare for 6.13.08 and 6.14.00

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 13
-#define PATCHLEVEL 07
-#define DATE "2005-01-18"
+#define PATCHLEVEL 08
+#define DATE "2005-03-03"
 
 #endif /* _h_patchlevel */
