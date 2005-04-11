0f8196e8751cb910b976ca984ec691982b9d8cf9
Kimmo Suominen <kim@tcsh.org>
2005-04-11 22:53:17 +0000

    Release 6.14.01

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 14
-#define PATCHLEVEL 0
-#define DATE "2005-03-25"
+#define PATCHLEVEL 1
+#define DATE "2005-04-11"
 
 #endif /* _h_patchlevel */
