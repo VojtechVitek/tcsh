5e8b268e5c0a42c9defc2f95176b4d0a254d45cc
Christos Zoulas <christos@zoulas.com>
2006-08-25 17:49:57 +0000

    Welcome to 07

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 14
-#define PATCHLEVEL 6
-#define DATE "2006-08-24"
+#define PATCHLEVEL 7
+#define DATE "2006-08-25"
 
 #endif /* _h_patchlevel */
