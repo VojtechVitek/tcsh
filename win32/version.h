22668917bc6d1b9d4eeca3175534fc595e4c2b2e
Amol Deshpande <amold@microsoft.com>
2008-09-10 20:34:21 +0000

    Fixed regression in GUI app detection. Created Visual Studio 2008 project. Still uses makefile to create some headers.

--- win32/version.h
+++ win32/version.h
@@ -5,9 +5,9 @@
 /* remember to change both instance of the version -amol */
 
 #ifdef NTDBG
-#define LOCALSTR ",nt-rev-8.09-debug"
+#define LOCALSTR ",nt-rev-8.10-debug"
 #else
-#define LOCALSTR ",nt-rev-8.09" 
+#define LOCALSTR ",nt-rev-8.10" 
 								//patches
 #endif NTDBG
 
