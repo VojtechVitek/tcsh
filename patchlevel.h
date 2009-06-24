9696cf90a3ddbcc50cd35d9e4467e7702a4c97df
Christos Zoulas <christos@zoulas.com>
2009-06-24 16:08:28 +0000

    ready for 6.16.01

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 16
-#define PATCHLEVEL 0
-#define DATE "2008-09-30"
+#define PATCHLEVEL 1
+#define DATE "2009-06-21"
 
 #endif /* _h_patchlevel */
