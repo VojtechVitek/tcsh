f46add1eaa72323d7937488e57c2a9d5428ac599
Christos Zoulas <christos@zoulas.com>
2010-05-06 14:27:26 +0000

    bump to 6.17.01

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 17
-#define PATCHLEVEL 0
-#define DATE "2010-04-21"
+#define PATCHLEVEL 1
+#define DATE "2010-05-06"
 
 #endif /* _h_patchlevel */
