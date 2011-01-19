910e7975d069df8a62feaf156922c777b2d49147
Christos Zoulas <christos@zoulas.com>
2011-01-19 03:03:47 +0000

    tcsh-6.17.04

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 17
-#define PATCHLEVEL 3
-#define DATE "2011-01-17"
+#define PATCHLEVEL 4
+#define DATE "2011-01-18"
 
 #endif /* _h_patchlevel */
